beb36329746ec5aa3e013c5a66787feb9c6dd705
Fernando Martinez <fernando817mm@gmail.com>
2021-09-17 15:12:57 -0500

    testing for both endpoints complete

--- api/middleware/payload.js
+++ api/middleware/payload.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
   const { username } = req.body;
   const { password } = req.body;
   !username || !password
-    ? res.status(401).json({
+    ? res.status(400).json({
         message: "username and password required",
       })
     : next();
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,60 @@
-// Write your tests here
+const request = require("supertest");
+const server = require("./server");
+
+const db = require("../data/dbConfig");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
 test("sanity", () => {
   expect(true).toBeTruthy();
+});
+
+describe("REGISTER | /api/auth/register", () => {
+  test("responds with correct object on success", async () => {
+    const inserted = await request(server).post("/api/auth/register").send({
+      username: "fernando",
+      password: "martinez",
+    });
+    expect(inserted.body).toMatchObject({ username: "fernando" });
+  });
+  test("responds with correct error message on incorrect payload", async () => {
+    const inserted = await request(server).post("/api/auth/register").send({
+      name: "fernando",
+    });
+    expect(inserted.text).toEqual(
+      '{"message":"username and password required"}'
+    );
+  });
+});
+
+describe("LOGIN | /api/auth/login", () => {
+  test("responds with correct welcome message", async () => {
+    await request(server).post("/api/auth/register").send({
+      username: "fernando",
+      password: "martinez",
+    });
+    const inserted = await request(server).post("/api/auth/login").send({
+      username: "fernando",
+      password: "martinez",
+    });
+    expect(inserted.body).toMatchObject({ message: "welcome, fernando" });
+  });
+  test("responds with correct error message on incorrect payload", async () => {
+    await request(server).post("/api/auth/register").send({
+      username: "fernando",
+      password: "martinez",
+    });
+    const inserted = await request(server).post("/api/auth/login").send({
+      username: "fernando",
+      password: "asdfsd",
+    });
+    expect(inserted.text).toEqual('{"message":"invalid credentials"}');
+  });
 });
